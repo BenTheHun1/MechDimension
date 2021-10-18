7f3f361a49f069cf7260f8fb30c58c4b1274b039
Benjamin Hunsicker <benhunsicker1@gmail.com>
2021-10-18 18:08:44 -0400

    Basic Fuel

--- Mech Dimension/Assets/Scripts/Fuel.cs
+++ Mech Dimension/Assets/Scripts/Fuel.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Fuel : MonoBehaviour
 {
+    public Image fuelDisplay;
+    public float fuel;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,6 @@ public class Fuel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        fuelDisplay.fillAmount = fuel;
     }
 }
