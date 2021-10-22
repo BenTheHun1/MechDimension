eaa635302206f230aa8cb1040c2c34387102e7d3
David Marshall <dmarsh12@student.fitchburgstate.edu>
2021-10-22 11:01:00 -0400

    music puush and stuff

---
+++ Mech Dimension/Assets/Scripts/musicSrc.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicSrc : MonoBehaviour
+{
+    public AudioClip iceTheme;
+    public AudioClip SiFiTheme;
+    public AudioClip hubTheme;
+    public AudioClip forestTheme;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void playIceTheme()
+    {
+
+    }
+
+}
